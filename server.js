d5fb37118b1dd8b9d593f7ef6bbe4b41886cd490
Quadrilateral <amandacdulaney@gmail.com>
2022-03-21 20:42:23 -0400

    app.get *

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 //GET request for notes
 app.get('/api/notes', (req, res) => {
 
